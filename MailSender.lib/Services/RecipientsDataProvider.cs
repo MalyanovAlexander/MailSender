e17da1fe0d428241ebf6aab311ab97b328aaac12
MalyanovAlexander <malyanov_91@mail.ru>
2021-04-04 22:43:57 +0300

    Представление информации о получателях почты

---
+++ MailSender.lib/Services/RecipientsDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using MailSender.lib.Data.LINQtoSQL;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MailSender.lib.Services
+{
+    public class RecipientsDataProvider
+    {
+        private readonly MailSenderDBDataContext _db;
+
+        public RecipientsDataProvider(MailSenderDBDataContext db) { _db = db; }
+
+        public IEnumerable<Recipient> GetAll() => _db.Recipient.ToArray();
+    }
+}
---
+++ MailSender/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using GalaSoft.MvvmLight;
+using MailSender.lib.Data.LINQtoSQL;
+using MailSender.lib.Services;
+using System.Collections.ObjectModel;
+
+namespace MailSender.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private RecipientsDataProvider _RecipientsProvider;
+        private string _WindowTitle = "Рассыльщик почты v0.1";
+
+        public string WindowTitle
+        {
+            get => _WindowTitle;
+            set => Set(ref _WindowTitle, value);
+        }
+
+        public ObservableCollection<Recipient> Recipients { get; } = new ObservableCollection<Recipient>();
+
+        public MainWindowViewModel(RecipientsDataProvider RecipientsProvider)
+        {
+            _RecipientsProvider = RecipientsProvider;
+
+            RefreshData();
+        }
+
+        private void RefreshData()
+        {
+            var recipients = Recipients;
+            recipients.Clear();
+            foreach (var recipient in _RecipientsProvider.GetAll())
+            {
+                recipients.Add(recipient);
+            }
+        }
+    }
+}
