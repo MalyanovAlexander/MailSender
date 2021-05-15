94486970b2e2bc9297d551fa2bfaaa4f5c4c1e34
MalyanovAlexander <malyanov_91@mail.ru>
2021-05-16 02:46:48 +0300

    Пример использования класса ConcurrentDictionary для создания пула Xml-сериализаторов

---
+++ MailSender.lib/Service/XMLSerializerPool.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MailSender.lib.Service
+{
+    public static class XMLSerializerPool
+    {
+        private static readonly ConcurrentDictionary<Type, XmlSerializer> _SerializerPool = new ConcurrentDictionary<Type, XmlSerializer>();
+
+        public static XmlSerializer GetSerializer<T>() => GetSerializer(typeof(T));
+
+        public static XmlSerializer GetSerializer(Type ObjectType) =>
+            _SerializerPool.GetOrAdd(ObjectType, type => new XmlSerializer(type));
+    }
+}
