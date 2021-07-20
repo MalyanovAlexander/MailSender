f7a81c5a10e501765dfd4f287ccb211fe71f092f
MalyanovAlexander <malyanov_91@mail.ru>
2021-07-20 11:54:11 +0300

    Добавление двух тестовых проектов, добавление EF6 к консольному тестовому проекту

---
+++ MailSender.ConsoleTest2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.ConsoleTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
