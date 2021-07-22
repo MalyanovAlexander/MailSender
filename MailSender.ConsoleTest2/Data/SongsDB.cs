0b4e17d2fcd3b0f5173dcf5a15e972403b4e1e3b
MalyanovAlexander <malyanov_91@mail.ru>
2021-07-22 16:09:03 +0300

    Создаём контекст БД + сущности, описывающие таблицы

---
+++ MailSender.ConsoleTest2/Data/SongsDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.ConsoleTest2.Data
+{
+    public class SongsDB : DbContext
+    {
+
+    }
+
+    public class Track
+    {
+
+    }
+
+    public class Artist
+    {
+
+    }
+}
