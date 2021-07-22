05c8825855fff798d7c5fa930561e79a7e3c3af5
MalyanovAlexander <malyanov_91@mail.ru>
2021-07-22 16:18:43 +0300

    Описание сущностей БД

--- MailSender.ConsoleTest2/Data/SongsDB.cs
+++ MailSender.ConsoleTest2/Data/SongsDB.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
@@ -12,13 +14,24 @@ namespace MailSender.ConsoleTest2.Data
 
     }
 
+    [Table("Track")]
     public class Track
     {
+        public int Id { get; set; }
 
+        [Required, MinLength(3)]
+        public string Name { get; set; }
+
+        public int Length { get; set; }
     }
 
     public class Artist
     {
+        public int Id { get; set; }
 
+        [Required, MinLength(3)]
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
     }
 }
