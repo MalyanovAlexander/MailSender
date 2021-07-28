789ee8381fbb0af46d5102ce5f2d6d970cb494b2
MalyanovAlexander <malyanov_91@mail.ru>
2021-07-28 22:24:54 +0300

    Первая миграция

---
+++ MailSender.ConsoleTest2/Data/Migrations/202107281918098_Initial.cs
@@ -0,0 +1,44 @@
+﻿namespace MailSender.ConsoleTest2.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    //В консоли диспетчера пакетов: Add-Migration Initial
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Artists",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Birthday = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Track",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Length = c.Int(nullable: false),
+                        Artist_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Artists", t => t.Artist_Id)
+                .Index(t => t.Artist_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Track", "Artist_Id", "dbo.Artists");
+            DropIndex("dbo.Track", new[] { "Artist_Id" });
+            DropTable("dbo.Track");
+            DropTable("dbo.Artists");
+        }
+    }
+}
