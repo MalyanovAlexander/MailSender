d8458d4b598452f7e88e6e6b27a92b2cfb743544
MalyanovAlexander <malyanov_91@mail.ru>
2021-07-28 22:15:26 +0300

    Включение механизма миграций

---
+++ MailSender.ConsoleTest2/Data/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+﻿namespace MailSender.ConsoleTest2.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    //В консоли диспетчера пакетов NuGet: Enable-Migrations -MigrationsDirectory Data\Migrations 
+    internal sealed class Configuration : DbMigrationsConfiguration<SongsDB>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            MigrationsDirectory = @"Data\Migrations";
+        }
+
+        protected override void Seed(SongsDB context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
